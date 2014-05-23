d4cc2fe52755779e8a9588ec2b8b8709b4fbb9c8
Michelle <michelle.chan@cslab4-25.stuy.edu>
2014-05-23 15:13:38 -0400

    updated customer

--- Customer.java
+++ Customer.java
@@ -2,6 +2,12 @@ public class Customer{
 
     private int points;
     private int numPeople;
-    
+    private int happiness;
+
+    public Customer(){
+	happiness = 10;
+	//max = 10, decrease when they get impatient
+	//TIME
+	numPeople = 
 
 }
