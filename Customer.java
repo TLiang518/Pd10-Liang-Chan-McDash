5e97368622380e374d2c298644d416d46932d2d6
Michelle <ellehcimx123@gmail.com>
2014-05-23 15:20:46 -0400

    updated customer

--- Customer.java
+++ Customer.java
@@ -8,6 +8,24 @@ public class Customer{
 	happiness = 10;
 	//max = 10, decrease when they get impatient
 	//TIME
-	numPeople = 
+	numPeople = (int)(Math.random()*4);
+    }
+    
+    public void consume(int order){
+    	if (happiness != 10){
+    		happiness ++;
+    	}
+    }
+
+    
+    public void exit(){
+    	points = happiness * happiness;
+    	//remove customer
+    }
+    
+    
+    	}
+    	}
+    }
 
 }
