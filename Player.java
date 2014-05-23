68a1677d83c58e352506b57af37cac9dd3be0ed1
Michelle <michelle.chan@cslab4-25.stuy.edu>
2014-05-23 15:11:06 -0400

    updated player class

--- Player.java
+++ Player.java
@@ -3,6 +3,17 @@ public class Player{
     private int points;
     private int level;
     private int experience;
+    private int holdOrder;
     
-    
+    public Player(){
+	points = 0;
+	level = 1;
+	experience = 10;
+	holdOrder = 0;
+    }
+
+    public void getFood(int order){
+	holdOrder = order;
+    }
+
 }
