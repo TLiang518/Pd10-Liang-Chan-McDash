3cacad4ecc5a4c980fb0c127b15733d075875b7e
TLiang518 <terranceliang01@gmail.com>
2014-05-27 04:29:08 -0400

    Updated main game

--- McDash.java
+++ McDash.java
@@ -1,9 +1,27 @@
+import java.util.*;
+import java.io.*;
+
 public class McDash{
 
-  //this is the main game file
-  //setup: four tables, all with four seats
+    //this is the main game file
+    //setup: four tables, all with four seats
     
+    public void setup(){
+	String n = "" //some method of finding name of player (scanner?)
+	Player p = new Player(n);
+	
+    }
 
+    public void game(Player p){
+	//game revolves around player, maybe we can save players too 
+	Customer[] newC = new Customer[4]; 
+	int newCusts = (int) Math.Random()*4;
+	for (int i = 0; i < newCusts ; i ++){
+	    newC[i] = new Customer();
+	}
+	//creates up to 4 random customers at a time
+	
+    }
 
 
 
