d44c924cb0f7fad9520c94f61d22bc209302541b
Michelle <ellehcimx123@gmail.com>
2014-06-08 19:50:37 -0400

    update

--- Diner/Customer.java
+++ Diner/Customer.java
@@ -4,56 +4,52 @@ public class Customer{
     private int points;
     private int numPeople;//maybe we should just keep it simple and make it a set # of peeps
     private int happiness; //correlates with time
-    private int order;
+    private static int order = (int)(Math.random()*4);
     private int image;
   
 
     
     public Customer(Player newP){
-	p = newP;
-	happiness = 10;
-	//max = 10, decrease when they get impatient
-	//TIME
-	image = (int)(Math.random()*5);
-	order = (int)(Math.random()*5);
-	numPeople = (int)(Math.random()*4);
+  p = newP;
+  happiness = 10;
+  //max = 10, decrease when they get impatient
+  //TIME
+  image = (int)(Math.random()*5);
+  order = (int)(Math.random()*5);
+  numPeople = (int)(Math.random()*4);
     }
     
     public int getImage(){
-	return image;
+  return image;
     }
 
     public int consume(int gOrder){
-	if (gOrder == order){
-	    if (happiness != 10){
-    		happiness ++;
-		return -1;
-	    }
-	}
-	if (happiness > 0){
-	    happiness --;
-	}
-	return order;
+  if (gOrder == order){
+      if (happiness != 10){
+        happiness ++;
+    return -1;
+      }
+  }
+  if (happiness > 0){
+      happiness --;
+  }
+  return order;
     }
 
     public void exit(){
-    	points = happiness * happiness;
-	p.getPoints(points);
-    	//remove customer
-    }	
+      points = happiness * happiness;
+  p.getPoints(points);
+      //remove customer
+    }  
     
     public void angry(){
-    	if(happiness < 4){
-    	   exit();
-    	}
-    	
+      if(happiness < 4){
+         exit();
+      }
+      
     }
 
-     public static int giveOrder(){
-     int order = (int)(Math.random()*4);
+     public static int giveOrder(){     
        return order;
     }
 }
-
-
- 
